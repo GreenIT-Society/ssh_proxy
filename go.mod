ebe4bf22de92cffa61041bda3f785f3a3d4b0180
Matisse Brard <dev@saaas.io>
2020-10-06 12:45:35 +0200

    Chgmt

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/gliderlabs/ssh v0.3.0
+	github.com/kr/pty v1.1.8
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
--- main.go
+++ main.go
@@ -1,36 +1,118 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
-	"os/exec"
+	"net"
+	"os"
+	"strings"
 
-	"github.com/gliderlabs/ssh"
+	"golang.org/x/crypto/ssh"
 )
 
-func main() {
-	ssh.Handle(func(s ssh.Session) {
-		cmd := exec.Command("ssh-add", "-l")
-		if ssh.AgentRequested(s) {
-			l, err := ssh.NewAgentListener()
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer l.Close()
-			go ssh.ForwardAgentConnections(l, s)
-			cmd.Env = append(s.Environ(), fmt.Sprintf("%s=%s", "SSH_AUTH_SOCK", l.Addr().String()))
-			log.Println(l.Addr().String())
-		} else {
-			cmd.Env = s.Environ()
-		}
-		cmd.Stdout = s
-		cmd.Stderr = s.Stderr()
-		if err := cmd.Run(); err != nil {
-			log.Println(err)
-			return
-		}
-	})
+var (
+	hostPrivateKeySigner ssh.Signer
+)
 
-	log.Println("starting ssh server on port 2222...")
-	log.Fatal(ssh.ListenAndServe(":2222", nil))
+func init() {
+	keyPath := "./host_key"
+	if os.Getenv("HOST_KEY") != "" {
+		keyPath = os.Getenv("HOST_KEY")
+	}
+
+	hostPrivateKey, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		panic(err)
+	}
+
+	hostPrivateKeySigner, err = ssh.ParsePrivateKey(hostPrivateKey)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func keyAuth(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+	log.Println(conn.RemoteAddr(), "authenticate with", key.Type())
+	return nil, nil
+}
+
+func main() {
+	config := ssh.ServerConfig{
+		PublicKeyCallback: keyAuth,
+	}
+	config.AddHostKey(hostPrivateKeySigner)
+
+	port := "2222"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	socket, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := socket.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		// From a standard TCP connection to an encrypted SSH connection
+		sshConn, newChans, _, err := ssh.NewServerConn(conn, &config)
+		if err != nil {
+			panic(err)
+		}
+		defer sshConn.Close()
+
+		log.Println("Connection from", sshConn.RemoteAddr())
+		go func() {
+			for chanReq := range newChans {
+				go handleChanReq(chanReq)
+			}
+		}()
+	}
+}
+
+func handleChanReq(chanReq ssh.NewChannel) {
+	if chanReq.ChannelType() != "session" {
+		chanReq.Reject(ssh.Prohibited, "channel type is not a session")
+		return
+	}
+
+	ch, reqs, err := chanReq.Accept()
+	if err != nil {
+		log.Println("fail to accept channel request", err)
+		return
+	}
+
+	req := <-reqs
+	if req.Type != "exec" {
+		ch.Write([]byte("request type '" + req.Type + "' is not 'exec'\r\n"))
+		ch.Close()
+		return
+	}
+
+	handleExec(ch, req)
+}
+
+// Payload: int: command size, string: command
+func handleExec(ch ssh.Channel, req *ssh.Request) {
+	command := string(req.Payload[4:])
+	log.Println(command)
+	gitCmds := []string{"git-receive-pack", "git-upload-pack"}
+
+	valid := false
+	for _, cmd := range gitCmds {
+		if strings.HasPrefix(command, cmd) {
+			valid = true
+		}
+	}
+	if !valid {
+		ch.Write([]byte("command is not a GIT command\r\n"))
+		ch.Close()
+		return
+	}
+
+	ch.Write([]byte("well done!\r\n"))
+	ch.Close()
 }
